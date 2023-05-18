9d75251b27fe4cf548af78d2443d1e3f92869aee
iamgp <iamgpower3@gmail.com>
2023-05-18 23:37:08 +0100

    updated controller with modifications

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,40 @@
-const getAllTasks = (req, res) => {
-	res.send('Get all tasks');
+const Task = require('../model/Task');
+
+const getAllTasks = async (req, res) => {
+	try {
+		const tasks = await Task.find({});
+		res.status(200).json({ tasks });
+	} catch (error) {
+		res.status(400).json({ success: false, msg: error });
+	}
+	// res.send('Get all tasks');
 };
 
-const getTask = (req, res) => {
-	res.send('Get single task');
+const getTask = async (req, res) => {
+	try {
+		const { id: taskID } = req.params;
+		const task = await Task.findOne({ _id: taskID });
+		if (!taskID) {
+			return res
+				.status(400)
+				.json({ success: false, msg: `no task with id ${taskID}` });
+		}
+		res.status(200).json({ task });
+	} catch (error) {
+		res.status(400).json({ success: false, msg: error });
+	}
+	// res.send('Get single task');
 };
 
-const createTask = (req, res) => {
-	res.json(req.body);
+const createTask = async (req, res) => {
+	try {
+		const task = await Task.create(req.body);
+		res.status(201).json({ task });
+	} catch (error) {
+		res.status(400).json({ success: false, msg: error });
+		// console.log(error);
+	}
+	// res.json(req.body);
 };
 
 const updateTask = (req, res) => {
--- model/Task.js
+++ model/Task.js
@@ -1,8 +1,16 @@
 const { model, Schema } = require('mongoose');
 
 const TaskSchema = new Schema({
-	name: String,
-	completed: Boolean,
+	name: {
+		type: String,
+		required: [true, 'please provide a task name '],
+		trim: true,
+		maxlength: [50, 'task name must not be more than 50 characters'],
+	},
+	completed: {
+		type: Boolean,
+		default: false,
+	},
 	date: { type: Date, default: Date.now },
 });
 
