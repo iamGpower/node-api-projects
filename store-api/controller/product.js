498a2c4cc7d7cec2c4ec0aa60d4f08f73ae0d102
iamgp <iamgpower3@gmail.com>
2023-06-01 21:39:45 +0100

    implemented sort and select query logic

--- store-api/controller/product.js
+++ store-api/controller/product.js
@@ -2,7 +2,7 @@ const Product = require('../model/Product');
 
 const getAllProducts = async (req, res) => {
 	// console.log(req.query);
-	const { featured, company, name } = req.query;
+	const { featured, company, name, sort, fields } = req.query;
 	const queryObj = {};
 
 	if (featured) queryObj.featured = featured === 'true' ? true : false;
@@ -12,8 +12,22 @@ const getAllProducts = async (req, res) => {
 	if (name) queryObj.name = { $regex: name, $options: 'i' };
 
 	console.log(queryObj);
-	// const products = await Product.find(req.query);
-	const products = await Product.find(queryObj).sort('-name -price');
+
+	let result = Product.find(queryObj);
+
+	if (sort) {
+		let sortList = sort.split(',').join(' ');
+		result = result.sort(sortList);
+	} else {
+		result = result.sort('createdAt');
+	}
+
+	if (fields) {
+		let sortList = fields.split(',').join(' ');
+		console.log(sortList);
+		result = result.select(sortList);
+	}
+	const products = await result;
 	res
 		.status(200)
 		.json({ status: 'success', nbHits: products.length, msg: products });
