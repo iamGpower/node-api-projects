921fa752a877cc48568200231b52fee4a69e377a
iamgp <iamgpower3@gmail.com>
2023-06-01 20:50:02 +0100

    gitignore fix

--- store-api/controller/product.js
+++ store-api/controller/product.js
@@ -1,27 +1,9 @@
 const Product = require('../model/Product');
 
 const getAllProducts = async (req, res) => {
-	// throw new Error('tasting async errors');
-	// const products = await Product.find({});
-	// const products = await Product.find({ isFeatured: false });
 	// console.log(req.query);
 	const { featured, company, name } = req.query;
 	const queryObj = {};
-
-	// switch (req.query) {
-	// 	case featured:
-	// 		queryObj.featured = featured === 'true' ? true : false;
-	// 		break;
-	// 	case company:
-	// 		queryObj.company = company;
-	// 		break;
-	// 	case name:
-	// 		queryObj.name = name;
-	// 		break;
-
-	// 	default: queryObj;
-	// 		break;
-	// }
 
 	if (featured) queryObj.featured = featured === 'true' ? true : false;
 
@@ -31,7 +13,7 @@ const getAllProducts = async (req, res) => {
 
 	console.log(queryObj);
 	// const products = await Product.find(req.query);
-	const products = await Product.find(queryObj);
+	const products = await Product.find(queryObj).sort('-name -price');
 	res
 		.status(200)
 		.json({ status: 'success', nbHits: products.length, msg: products });
