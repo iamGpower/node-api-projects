ab36746c514b397394edbc8dacddb2f0f80814bf
iamgp <iamgpower3@gmail.com>
2023-06-29 08:03:36 +0100

    Express additional config for rate limiter middleware

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -18,11 +18,19 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1);
+
+app.use(
+	ratelimiter({
+		windowMs: 15 * 60 * 1000, // 15 Mins
+		max: 100, // limit each IP to 100 requests per windowsMs
+	}),
+);
+
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-app.use(ratelimiter());
 
 // routes
 app.use('/api/v1/auth', authRouter);
