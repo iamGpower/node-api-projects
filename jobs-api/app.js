5fe7efc8d7b662848bc528fbc86b99c142890ead
iamgp <iamgpower3@gmail.com>
2023-06-29 16:02:55 +0100

    Modified api calls limit to 30 mins per 100 calls

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -4,7 +4,6 @@ require('express-async-errors');
 // security packages
 const helmet = require('helmet');
 const cors = require('cors');
-const xss = require('xss-clean');
 const ratelimiter = require('express-rate-limit');
 
 const express = require('express');
@@ -18,26 +17,43 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.set('trust proxy', 1);
+// app.set('trust proxy', 1);
 
-app.use(
-	ratelimiter({
-		windowMs: 15 * 60 * 1000, // 15 Mins
-		max: 100, // limit each IP to 100 requests per windowsMs
-	}),
-);
+// app.use(
+// 	ratelimiter({
+// 		windowMs: 15 * 60 * 1000, // 15 Mins
+// 		max: 100, // limit each IP to 100 requests per windowsMs
+// 	}),
+// );
+
+const apiLimiter = ratelimiter({
+	windowMs: 30 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	message:
+		'Too many request made from this IP, please try again after 30 minutes',
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+const createAccountLimiter = ratelimiter({
+	windowMs: 60 * 60 * 1000, // 1 hour
+	max: 5, // Limit each IP to 5 create account requests per `window` (here, per hour)
+	message:
+		'Too many accounts created from this IP, please try again after an hour',
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
-app.use(xss());
 
 app.get('/', (req, res) => {
 	res.send(`<h2>🎉 Welcome to Jobs API 😊 </h2>`);
 });
 // routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', authMiddleware, jobsRouter);
+app.use('/api/v1/jobs', apiLimiter, authMiddleware, jobsRouter);
 
 // error handler
 app.use(notFoundMiddleware);
